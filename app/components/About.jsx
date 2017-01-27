10c9d047b205e2bf2476b08ba9b0ec87473a23b7
Kevin Carr <cochewen@gmail.com>
2017-01-27 01:09:02 -0500

    Updated the About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 // var About = React.createClass({
 //   render:
 //   function () {
@@ -12,9 +12,40 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page. This app looks you know the local weather for any city you type in!</p>
+      <h1 className="text-center">About</h1>
+
+      <div className="row">
+        <div className="medium-4 columns">
+          <div className="card">
+            <div className="card-section">
+              <Link to="https://github.com/andrewjmead/react-course-weather-app">React Course Weather App</Link>
+              <p>Original Github repo for app.</p>
+            </div>
+          </div>
+        </div>
+        <div className="medium-4 columns">
+          <div className="card">
+            <div className="card-section">
+              <Link to="https://facebook.github.io/react/">Learn about React.js</Link>
+              <p>A JS Library for building User Interfaces created by Facebook.</p>
+            </div>
+          </div>
+        </div>
+        <div className="medium-4 columns">
+          <div className="card">
+            <div className="card-section">
+              <Link to="https://openweathermap.org/">Open Weather Map API</Link>
+              <p>The API we use to gather weather data.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
+
+
+
+
 
   );
 };
