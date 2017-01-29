f8453de504d9c01b27776c2a82807a9532dafa33
Kevin Carr <cochewen@gmail.com>
2017-01-28 22:23:26 -0500

    included a new app.css file for customized css and an ErrorModal created through foundation

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var ErrorModal = require('ErrorModal');
 var OpenWeatherMap = require('OpenWeatherMap');
 
 var Weather = React.createClass({
@@ -16,7 +17,10 @@ var Weather = React.createClass({
     var that = this;
 
 
-    this.setState({isLoading: true});
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
 
     OpenWeatherMap.getTemp(city).then(function (temp) {
       that.setState({
@@ -24,15 +28,17 @@ var Weather = React.createClass({
         temp: temp,
         isLoading: false
       });
-    }, function(errorMessage) {
-      that.setState({isLoading: false});
-      alert(errorMessage);
+    }, function(e) {
+      that.setState({
+        isLoading: false,
+        errorMessage: e.message
+      });
     })
   },
 
 
   render: function () {
-    var {isLoading, temp, city} = this.state;
+    var {isLoading, temp, city, errorMessage} = this.state;
 
     function renderMessage() {
       if (isLoading) {
@@ -42,11 +48,20 @@ var Weather = React.createClass({
       }
     }
 
+    function renderError() {
+      if(typeof errorMessage === 'string') {
+        return (
+          <ErrorModal message = {errorMessage}/>
+        )
+      }
+    }
+
     return (
       <div>
-        <h1 className="text-center">Get Weather</h1>
+        <h1 className="text-center page-title">Get Weather</h1>
         <WeatherForm onNewCity={this.processNewCity}/>
         {renderMessage()}
+        {renderError()}
 
       </div>
 
